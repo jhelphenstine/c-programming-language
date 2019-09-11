b9945428372512e1d01135a0e7e0b1793221d720
jhelphenstine <jhelphenstine@pm.me>
2019-09-11 17:20:16 -0400

    Put a copy of reverse into 5-6. Tomorrow I'll modify it to use pointers.

---
+++ chapter5/Exercise 5-6/reverse.c
@@ -0,0 +1,20 @@
+/* reverse: reverse a string
+    @params:    int length      -- strlen() results
+                char *target    -- string to reverse
+                int start       -- position to start from (usually 0)
+*/
+void reverse(int length, char target[], int x){
+    if(x < (length/2)){
+        /* We aren't past the halfway mark yet */
+        char swap;
+        swap = target[x];
+        target[x] = target[length-(x+1)];
+        target[length-(x+1)] = swap;
+        (x)++;
+        reverse(length, target, x);
+    }
+    else{
+        return;
+    }
+}
+
