b5ccec1ae6a697e9349d235f7cff33b6d791e3a6
justin <jhelphenstine@student.umuc.edu>
2019-09-11 11:52:54 -0400

    Exercise 5-6, getline. Cheated to see what was meant by 'use pointers instead' for this function.

---
+++ chapter5/Exercise 5-6/getline.c
@@ -0,0 +1,52 @@
+/* getline: get line into s, return length */
+#include <stdio.h>
+#include <string.h>	/* for strlen */
+
+#define MAX 100
+
+int getline_p(char *s, int lim);
+int getLine(char s[], int lim);
+
+
+int main()
+{
+	char s[MAX];
+	int length = getline_p(s, 20);
+	printf("%s has %d bytes\n", s, length);
+	return 0;
+}
+
+
+int getLine(char s[], int lim)
+{
+	int c, i;
+
+	i = 0;
+	while((--lim > 0) && ((c = getchar()) != EOF) && (c != '\n')){
+		s[i++] = c;
+	}
+	if (c == '\n'){
+		s[i++] = c;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+
+/* getline: get line into s, return length */
+/* this version uses pointers instead of array indices */
+int getline_p(char *s, int lim)
+{
+	int c;
+	char *p;
+
+	p = s;
+	while((--lim > 0) && ((c = getchar()) != EOF) && (c != '\n')){
+		*p++ = c;
+	}
+	if (c == '\n'){
+		*p++ = c;
+	}
+	*p = '\0';
+	return strlen(s);
+}
