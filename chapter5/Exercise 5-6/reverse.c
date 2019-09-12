94851c76c3975144991ff59d147e7eb015106f2a
justin <jhelphenstine@student.umuc.edu>
2019-09-12 09:38:08 -0400

    Exercise 5-6, reverse.c now includes reverse_p(). This version uses pointers, but does not use recursion -- combining the two seemed unnecessarily complicated.

--- chapter5/Exercise 5-6/reverse.c
+++ chapter5/Exercise 5-6/reverse.c
@@ -1,3 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+void reverse(int length, char target[], int x);
+void reverse_p(int length, char *target);
+
+int main()
+{
+    char test[] ="Hello World!";
+    reverse_p(strlen(test), test);
+    printf("Reversed: %s\n", test);
+    return 0;
+}
+
 /* reverse: reverse a string
     @params:    int length      -- strlen() results
                 char *target    -- string to reverse
@@ -18,3 +33,23 @@ void reverse(int length, char target[], int x){
     }
 }
 
+/* reverse: reverse a string
+    @params:    int length      -- strlen() results
+                char *target    -- string to reverse
+
+    This version uses pointers, but is not recursive.
+*/
+void reverse_p(int length, char *target)
+{
+    char *ptarget_a;
+    char *ptarget_b;
+    char swap;
+    for (int i = 0; i < length/2; i++){
+        ptarget_a = (target + i);
+        ptarget_b = (target + length - (i + 1)); /* minus 1 for NULL at end */
+        swap = *ptarget_b;
+        *ptarget_b = *ptarget_a;
+        *ptarget_a = swap;
+    }
+    return;
+}
