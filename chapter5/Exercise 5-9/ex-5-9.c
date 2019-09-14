3ad4f632a78c2fcf4d50072f47d9872922849cba
justin <jhelphenstine@student.umuc.edu>
2019-09-14 10:22:11 -0400

    Exercise 5-9: Refactor Ex. 5-8 to use pointers.

--- chapter5/Exercise 5-9/ex-5-9.c
+++ chapter5/Exercise 5-9/ex-5-9.c
@@ -6,25 +6,28 @@ static char daytab[2][13] = {
 };
 
 /* day_of_year: set day of year from month & day */
-int day_of_year(int year, int month, int day);
+int day_of_year(int year, int *month, int *day);
 
 /* month_day: set month, day from day of year */
-void month_day(int year, int yearday, int *pmonth, int *pday);
+void month_day(int *year, int *yearday, int *pmonth, int *pday);
 
 int main()
 {
-    int d = day_of_year(2019, 2, 28);
-    if(d == -1){
+    /* TEST DATA */
+    int day = 29;
+    int month = 3;
+    int year = 2019;
+
+    int yearday = day_of_year(2019, &month, &day);
+    if(yearday == -1){
         printf("Error - bad month provided. Must be between 1 & 12. \n");
         return 0;
     }
-    if(d == -2){
+    if(yearday == -2){
         printf("Error - bad day provided. Must be within 1 and [28,29,30,31] depending on month.\n");
         return 0;
     }
-    int month;
-    int day;
-    month_day(2019, d, &month, &day);
+    month_day(&year, &yearday, &month, &day);
     if(day == -1){
         printf("Error - bad day provided. Must be between 1 & [365,366] depending on year.\n");
         return 0;
@@ -33,39 +36,42 @@ int main()
     return 0;
 }
 
-int day_of_year(int year, int month, int day)
+int day_of_year(int year, int *month, int *day)
 {
     int i, leap;
+    int *pday = day;
+    int *pmonth = month;
 
-    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
     /* error checking */
-    if ((month > 12) || (month < 1)){
+    if ((*pmonth > 12) || (*pmonth < 1)){
         return -1;  /* invalid month */
     }
-    if ((day < 1) || (day > daytab[leap][month])){
+    if ((*pday < 1) || (*pday > daytab[leap][*pmonth])){
         return -2;  /* invalid day of month */
     }
-
-    for(i = 1; i < month; i++){
-        day += daytab[leap][i];
+    for(i = 1; i < *pmonth; i++){
+        *pday += daytab[leap][i];
     }
-    return day;
+    return *pday;
 }
 
-void month_day(int year, int yearday, int *pmonth, int *pday)
+void month_day(int *year, int *yearday, int *pmonth, int *pday)
 {
     int i, leap;
+    int *pyear = year;
+    int *pyearday = yearday;
 
-    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    leap = (*pyear%4 == 0 && *pyear%100 != 0) || *pyear%400 == 0;
 
-    for(i = 1; yearday > daytab[leap][i]; i++){
-        yearday -= daytab[leap][i];
+    for(i = 1; *pyearday > daytab[leap][i]; i++){
+        *pyearday -= daytab[leap][i];
     }
     *pmonth = i;
-    *pday = yearday;
+    *pday = *pyearday;
 
     /* Error handling */
-    if ((yearday > (365 + leap)) || (yearday < 1)){
+    if ((*pyearday > (365 + leap)) || (*pyearday < 1)){
         *pday = -1;  /* invalid yearday */
     }
     return;
