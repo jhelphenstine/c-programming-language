dccbf177e32699cb77067ba4ba3a61c5d195bcf6
justin <jhelphenstine@student.umuc.edu>
2019-09-17 21:34:14 -0400

    Exercise 5-9 is broken now. I'm missing something regarding pointers & multidimensional arrays.

--- chapter5/Exercise 5-9/ex-5-9.c
+++ chapter5/Exercise 5-9/ex-5-9.c
@@ -6,10 +6,13 @@ static char daytab[2][13] = {
 };
 
 /* day_of_year: set day of year from month & day */
-int day_of_year(int year, int *month, int *day);
+int day_of_year(int year, int month, int day);
 
 /* month_day: set month, day from day of year */
-void month_day(int *year, int *yearday, int *pmonth, int *pday);
+void month_day(int year, int yearday, int *pmonth, int *pday);
+
+/* is a year a leap year? */
+int is_leapyear(int year);
 
 // Driver code
 int main()
@@ -19,7 +22,7 @@ int main()
     int month = 3;
     int year = 2017;
 
-    int yearday = day_of_year(year, &month, &day);
+    int yearday = day_of_year(year, month, day);
     if(yearday == -1){
         printf("Error - bad month provided. Must be between 1 & 12. \n");
         return 0;
@@ -28,7 +31,7 @@ int main()
         printf("Error - bad day provided. Must be within 1 and [28,29,30,31] depending on month.\n");
         return 0;
     }
-    month_day(&year, &yearday, &month, &day);
+    month_day(year, yearday, &month, &day);
     if(day == -1){
         printf("Error - bad day provided. Must be between 1 & [365,366] depending on year.\n");
         return 0;
@@ -37,47 +40,45 @@ int main()
     return 0;
 }
 
-int day_of_year(int year, int *month, int *day)
+int is_leapyear(int year)
 {
-    int i, leap;
-    int *pday = day;
-    int *pmonth = month;
-    char (*pdaytab)[1] = daytab;
-
-    leap = ((year%4 == 0) && (year%100 != 0)) || (year%400 == 0);
-    pdaytab += (leap*13);
-    /* error checking */
-    if ((*pmonth > 12) || (*pmonth < 1)){
-        return -1;  /* invalid month */
-    }
-    if ((*pday < 1) || (*pday > *pdaytab[*pmonth])){
-        return -2;  /* invalid day of month */
-    }
-    for(i = 1; i < *pmonth; i++){
-        *pday += *pdaytab[i];
-    }
-    return *pday;
+    return ((year%4 == 0) && (year%100 != 0)) || (year%400 == 0);
 }
 
-void month_day(int *year, int *yearday, int *pmonth, int *pday)
+int day_of_year(int year, int month, int day)
+{
+    int i;
+    printf("leap year?: %d\n", is_leapyear(year));
+    char (*pdaytab)[13] = daytab[(is_leapyear(year))*13];
+
+    /* error checking */
+    if ((month > 12) || (month < 1)){
+        return -1;  /* invalid month */
+    }
+    if ((day < 1) || (day > pdaytab[month])){
+        return -2;  /* invalid day of month */
+    }
+    for(i = 1; i < month; i++){
+        day += pdaytab[i];
+    }
+    return day;
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pday)
 {
     int i, leap;
-    int *pyear = year;
-    int *pyearday = yearday;
-    char (*pdaytab)[1] = daytab;
 
-    leap = ((*pyear%4 == 0) && (*pyear%100 != 0)) || *pyear%400 == 0;
-    pdaytab += (leap*13);
+    char (*pdaytab)[13] = daytab[(is_leapyear(year)) * 13];
 
-    for(i = 1; *pyearday > daytab[leap][i]; i++){
-        *pyearday -= *pdaytab[i];
+    for(i = 1; yearday > daytab[leap][i]; i++){
+        yearday -= *pdaytab[i];
     }
     *pmonth = i;
-    *pday = *pyearday;
+    *pday = yearday;
 
 
     /* Error handling */
-    if ((*pyearday > (365 + leap)) || (*pyearday < 1)){
+    if ((yearday > (365 + leap)) || (yearday < 1)){
         *pday = -1;  /* invalid yearday */
     }
     return;
